1f2615613cf86805c8a7d1f7b7c75f49679434fa
vahid <vahid@DESKTOP-CUBC673>
2017-12-08 14:45:27 +0330

    add unit of work

---
+++ VDFramework.Data/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDFramework.Data
+{
+    public interface IUnitOfWork
+    {
+        IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+        int SaveChanges();
+    }
+}
